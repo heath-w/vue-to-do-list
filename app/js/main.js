2a1ba783310a76d54185ce6a52d9c2e60241e86e
Heath Williams <awilli01@amgen.com>
2017-10-05 16:47:25 -0400

    Almost there on list... buttons need style and actions

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,26 @@
 
-document.onreadystatechange = function() {	
-	if ( document.readyState === 'interactive' ) {
-		console.log( 'readyState is interactive' );
+// document.onreadystatechange = function() {	
+// 	if ( document.readyState === 'interactive' ) {
+// 		console.log( 'readyState is interactive' );
+// 	}
+// };
+
+var toDoPanel = new Vue( {
+	el : '#toDoPanel',
+	data : {
+		newToDoItem : '',
+		toDoListItems : [ 'Buy milk', 'Buy cheese' ]
+	},
+	methods : {
+		addToToDoItem : addToToDoItem
 	}
-};
+});
+
+function addToToDoItem( newItem ) {
+	console.log( 'addToToDoItem()' );
+	console.log( 'addToToDoItem() - this.newToDoItem: ', this.newToDoItem );
+
+	this.toDoListItems.push( this.newToDoItem );
+
+	this.newToDoItem = '';
+}
